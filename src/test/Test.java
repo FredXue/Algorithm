6b3e9b77a65e9cc03367088360d094dabf4d2960
FredXue <xuexiaoyu@meituan.com>
2016-08-11 15:48:36 +0800

    refresh

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,16 +1,16 @@
 import org.apache.commons.codec.binary.Base64;
 
 public class Test{
-    
+
     public static void main(String[] args){
 
         long unixTime = System.currentTimeMillis();
         long preTime = unixTime-1111;
         String rawString = preTime + "1111" + unixTime;
-        
+
         byte[] encoded = Base64.encodeBase64(rawString);
         String encodedString = new String(encoded);
-        
+
         System.out.println(encodedString);
     }
 }
